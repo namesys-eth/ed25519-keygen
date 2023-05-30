b002683878b869b47df4222a4f4bf4a829cb3512
sshmatrix <cryptosauron@protonmail.com>
2023-05-30 13:54:02 +0530

    compact comments & code

--- src/ipns.ts
+++ src/ipns.ts
@@ -7,31 +7,23 @@ const ADDRESS_VERSION = new Uint8Array([0x03]);
 
 // Formats IPNS public key in bytes array format to 'ipns://k...' string format
 export function formatPublicKey(pubBytes: Uint8Array) {
-  // Convert bytes array to hex string
-  const hexString = hex.encode(pubBytes);
-  // Convert hex string to BigInt
-  const pubBigInt = BigInt(`0x${hexString}`);
-  // Convert BigInt to base-36 string
-  const base36String = pubBigInt.toString(36);
-  // Return public key string in format: 'ipns://k' + base36String
-  return `ipns://k${base36String}`;
+  // Convert bytes array → hex string → BigInt → base-36 string → IPNS
+  return `ipns://k${BigInt(`0x${hex.encode(pubBytes)}`).toString(36)}`;
 }
 
 // Takes an IPNS pubkey (address) string as input and returns bytes array of the key
 // Supports various formats ('ipns://k', 'ipns://b', 'ipns://f')
 // Handles decoding and validation of the key before returning pubkey bytes
 export function parseAddress(address: string): Uint8Array {
-  // Verify if pubkey starts with 'ipns://' and remove 'ipns://' prefix
   if (address.startsWith('ipns://')) address = address.slice(7);
-  // Convert pubkey to lowercase
   address = address.toLowerCase();
   let hexKey;
   if (address.startsWith('k')) {
     const b36 = '0123456789abcdefghijklmnopqrstuvwxyz';
     let result = 0n;
-    // Iterate over chars in pubkey, starting from the second char
+    // Iterate over chars in pubkey and convert to BigInt
     for (let i = 1; i < address.length; ) {
-      // Convert each base-36 char to BigInt
+      // start at second char
       result = result * 36n + BigInt(b36.indexOf(address.charAt(i++)));
     }
     // Convert BigInt to hex format and pad it with zeros up to length 80
@@ -40,23 +32,19 @@ export function parseAddress(address: string): Uint8Array {
     // Decode base-32 pubkey (after removing 'b' prefix) and encode it as a hex string
     hexKey = hex.encode(base32.decode(address.slice(1).toUpperCase()));
   } else if (address.startsWith('f')) {
-    // Remove 'f' prefix from pubkey
     hexKey = address.slice(1);
-  } else throw new Error('Unsupported Base-X Format'); // Throw an error if pubkey format is not supported
+  } else throw new Error('Unsupported Base-X Format'); // Throw error if pubkey format is not supported
 
   // Check if hexKey has expected prefix '0172002408011220' and length of 80
   if (hexKey.startsWith('0172002408011220') && hexKey.length === 80) {
-    // Decode the hex key and return it as bytes array
     return hex.decode(hexKey);
   }
-
-  // Throw an error if IPNS key prefix is invalid
+  // Throw error if IPNS key prefix is invalid
   throw new Error('Invalid IPNS Key Prefix: ' + hexKey);
 }
 
 // Generates an ed25519 pubkey from a seed and converts it to several IPNS pubkey formats
 export async function getKeys(seed: Uint8Array) {
-  // Check if seed length = 32 bytes
   if (seed.length != 32) throw new TypeError('Seed must be 32 bytes in length');
   // Generate ed25519 public key from seed
   const pubKey = await ed25519.getPublicKey(seed);
@@ -65,7 +53,6 @@ export async function getKeys(seed: Uint8Array) {
     new Uint8Array([0x01, 0x72, 0x00, 0x24, 0x08, 0x01, 0x12, 0x20]),
     pubKey
   );
-  // Encode pubKeyBytes as a hex string and convert it to lowercase
   const hexKey = hex.encode(pubKeyBytes).toLowerCase();
   // Return different representations of the keys
   return {
